a1fd8a9584003410c99da3c1c7ecdef60ad528d8
Cranky-dev <130955083+Cranky-dev@users.noreply.github.com>
2024-08-27 14:56:00 -0500

    Update background.js

--- js/background.js
+++ js/background.js
@@ -2,7 +2,8 @@ function bgset() {
 let img = localStorage.getItem("image")
 if (img === null){}
 else {
-document.body.style.backgroundImage = "url(" + img + ")"
+document.body.style.backgroundImage = "url(" + img + ")";
+document.body.style.backgroundSize = "100% 100%";
 }
 let font = localStorage.getItem("font")
 if (font === null){}
